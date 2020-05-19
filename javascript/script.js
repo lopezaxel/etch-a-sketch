6c6afea128014f4ee7b26a4c78a5b7cc562f839f
axel <lopezaxelcarlos@gmail.com>
2020-05-19 11:55:06 -0300

    Added button

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,15 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 function createGrids(gridSize = 16) {
+  let grids = document.querySelectorAll('.gridDiv');
+  grids.forEach((grid) => {
+    gridContainer.removeChild(grid);
+  })
   for(let i = 1; i <= gridSize; i++) {
     for(let i = 1; i <= gridSize; i++) {
       let gridDiv = document.createElement('div');
       gridDiv.classList.add('gridDiv');
-      gridDiv.setAttribute('style', 'height: 32px; border: lightgray 1px solid')
+      gridDiv.setAttribute('style', 'border: lightgray 1px solid;')
       gridContainer.appendChild(gridDiv);
     }
   }
@@ -20,11 +24,15 @@ function changeColor() {
   });
 }
 
-const resetButton = document.querySelector('#button');
+function respondReset() {
+  let gridSize;
+  gridSize = prompt('How many ' + 'squares per side do you want?', 16);
+  createGrids(gridSize);
+}
 
-let gridSize;
-resetButton.addEventListener('click', () => gridSize = prompt('How many ' + 
-  'squares per side do you want?', 16));
+const resetButton = document.querySelector('#button');
+resetButton.addEventListener('click', respondReset);
 
 createGrids();
 changeColor();
+
