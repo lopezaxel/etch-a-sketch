e20c76b02edfb84c6df32bffc6178764ebfc8412
axel <lopezaxelcarlos@gmail.com>
2020-05-20 11:10:19 -0300

    Updated button to change grid size and random color

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,6 @@
-const gridContainer = document.querySelector('#gridContainer');
-
 function createGrids(gridSize = 16) {
-  let grids = document.querySelectorAll('.gridDiv');
-  grids.forEach((grid) => {
-    gridContainer.removeChild(grid);
-  })
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for(let i = 1; i <= gridSize; i++) {
     for(let i = 1; i <= gridSize; i++) {
       let gridDiv = document.createElement('div');
@@ -15,24 +11,42 @@ function createGrids(gridSize = 16) {
   }
 }
 
+function deleteOldGrids() {
+  let grids = document.querySelectorAll('.gridDiv');
+  grids.forEach((grid) => {
+    gridContainer.removeChild(grid);
+  })
+}
+
 function changeColor() {
-  const squareDivs = document.querySelectorAll('.gridDiv');
+  let squareDivs = document.querySelectorAll('.gridDiv');
   squareDivs.forEach((gridSquare) => {
     gridSquare.addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = 'black';
+      
+      e.target.style.backgroundColor = color;
     });
   });
+}
+
+function pickRandomColor() {
+  let color1 = Math.floor(Math.random() * 255);
+  let color2 = Math.floor(Math.random() * 255);
+  let color3 = Math.floor(Math.random() * 255);
+  return `rgb(${color1}, ${color2}, ${color3})`;
 }
 
 function respondReset() {
   let gridSize;
   gridSize = prompt('How many ' + 'squares per side do you want?', 16);
+  deleteOldGrids();
   createGrids(gridSize);
+  changeColor();
 }
 
+let colorRandom = pickRandomColor();
+const gridContainer = document.querySelector('#gridContainer');
 const resetButton = document.querySelector('#button');
 resetButton.addEventListener('click', respondReset);
 
 createGrids();
 changeColor();
-
